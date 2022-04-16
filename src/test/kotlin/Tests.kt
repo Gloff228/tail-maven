7c4bb71e6377c2f6dd7d889d77ef991a4049c156
Gloff228 <romo4ka101@gmail.com>
2022-04-16 10:54:22 +0300

    Fixes

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -7,93 +7,93 @@ import java.io.FileNotFoundException
 class Tests {
     @Test
     fun extractSymbols() {
-        extract(listOf("src/test/kotlin/input/123.txt"), "src/test/kotlin/output/output.txt", null, 2)
+        extract(listOf("src/test/resources/input/123.txt"), "src/test/resources/output/output.txt", null, 2)
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput1.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput1.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
 
         extract(
             listOf(
-                "src/test/kotlin/input/strLines.txt",
-                "src/test/kotlin/input/123.txt",
-                "src/test/kotlin/input/numLines.txt"
-            ), "src/test/kotlin/output/output.txt", null, 24
+                "src/test/resources/input/strLines.txt",
+                "src/test/resources/input/123.txt",
+                "src/test/resources/input/numLines.txt"
+            ), "src/test/resources/output/output.txt", null, 24
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput3.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput3.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
     }
 
     @Test
     fun extractStrings() {
         extract(
-            listOf("src/test/kotlin/input/numLines.txt", "src/test/kotlin/input/poem.txt"),
-            "src/test/kotlin/output/output.txt",
+            listOf("src/test/resources/input/numLines.txt", "src/test/resources/input/poem.txt"),
+            "src/test/resources/output/output.txt",
             5,
             null
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput2.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput2.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
 
         extract(
             listOf(
-                "src/test/kotlin/input/numString.txt",
-                "src/test/kotlin/input/123.txt",
-                "src/test/kotlin/input/poem.txt"
-            ), "src/test/kotlin/output/output.txt", 10, null
+                "src/test/resources/input/numString.txt",
+                "src/test/resources/input/123.txt",
+                "src/test/resources/input/poem.txt"
+            ), "src/test/resources/output/output.txt", 10, null
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput4.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput4.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
     }
 
     @Test
     fun zeroCountOfSymbols() {
         extract(
             listOf(
-                "src/test/kotlin/input/numString.txt",
-                "src/test/kotlin/input/123.txt",
-                "src/test/kotlin/input/poem.txt"
-            ), "src/test/kotlin/output/output.txt", null, 0
+                "src/test/resources/input/numString.txt",
+                "src/test/resources/input/123.txt",
+                "src/test/resources/input/poem.txt"
+            ), "src/test/resources/output/output.txt", null, 0
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput5.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput5.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
     }
 
     @Test
     fun zeroCountOfString() {
         extract(
             listOf(
-                "src/test/kotlin/input/numString.txt",
-                "src/test/kotlin/input/123.txt",
-                "src/test/kotlin/input/poem.txt"
-            ), "src/test/kotlin/output/output.txt", 0, null
+                "src/test/resources/input/numString.txt",
+                "src/test/resources/input/123.txt",
+                "src/test/resources/input/poem.txt"
+            ), "src/test/resources/output/output.txt", 0, null
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput5.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput5.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
     }
 
     @Test
     fun fileNotFound() {
         assertThrows(FileNotFoundException::class.java) {
             extract(
-                listOf("src/test/kotlin/input/fileNotFound.txt"),
-                "src/test/kotlin/output/outputNotFound.txt",
+                listOf("src/test/resources/input/fileNotFound.txt"),
+                "src/test/resources/output/outputNotFound.txt",
                 null,
                 12345
             )
@@ -104,8 +104,8 @@ class Tests {
     fun invalidFileFormat() {
         assertThrows(IllegalArgumentException::class.java) {
             extract(
-                listOf("src/test/kotlin/input/word.docx"),
-                "src/test/kotlin/output/outputNotFound.txt",
+                listOf("src/test/resources/input/word.docx"),
+                "src/test/resources/output/outputNotFound.txt",
                 null,
                 12345
             )
@@ -135,13 +135,13 @@ class Tests {
                 "123456789",
                 "qwerty",
                 "qazqazqazqaz1"
-            ), "src/test/kotlin/output/output.txt", null, 7
+            ), "src/test/resources/output/output.txt", null, 7
         )
         assertEquals(
-            File("src/test/kotlin/output/expectedOutput6.txt").readLines(),
-            File("src/test/kotlin/output/output.txt").readLines()
+            File("src/test/resources/output/expectedOutput6.txt").readLines(),
+            File("src/test/resources/output/output.txt").readLines()
         )
-        File("src/test/kotlin/output/output.txt").delete()
+        File("src/test/resources/output/output.txt").delete()
     }
 }
 
